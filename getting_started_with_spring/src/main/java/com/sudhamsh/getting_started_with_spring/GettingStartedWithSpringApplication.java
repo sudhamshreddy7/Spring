465fb69670bfa84528a480c089dadbd4b2e10049
sudhamshreddy7 <sudhamshreddy06@gmail.com>
2024-12-26 10:24:53 -0500

    first commit

---
+++ getting_started_with_spring/src/main/java/com/sudhamsh/getting_started_with_spring/GettingStartedWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sudhamsh.getting_started_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringApplication.class, args);
+	}
+
+}
---
+++ getting_started_with_spring/src/test/java/com/sudhamsh/getting_started_with_spring/GettingStartedWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhamsh.getting_started_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
